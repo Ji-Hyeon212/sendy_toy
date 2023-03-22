31b2bbf5d5b82b572646b4867e706aab58a1ee1d
jihyeon <wlgus7464@gmail.com>
2023-03-22 16:27:50 +0900

    날짜, 운반 옵션에 따른 가격 계산

--- src/components/Address/Cost.tsx
+++ src/components/Address/Cost.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 const REQUEST_URL = "https://api.sendy.ai/guest/fare/predict";
 const API_KEY =
   "5cd0fbf6ca11f40e14a632124e94cabe9709752fb1484d137b0ef516d3bb0e25";
+const date = new Date();
 
 interface costType {
   distance: string;
@@ -18,7 +19,7 @@ interface costType {
 
 const Cost = (props: costType) => {
   const [cost, setCost] = useState("");
-  const data = {
+  const [data, setData] = useState({
     distance: props.distance,
     pickUpDateTime: "2023-03-21T12:00:00",
     truckInfo: {
@@ -41,7 +42,55 @@ const Cost = (props: costType) => {
       },
     ],
     workType: "LOADING",
-  };
+  });
+
+  useEffect(() => {
+    let pickUpDateTime: string;
+    const year = String(date.getFullYear());
+    let month = String(date.getMonth() + 1);
+    let day = String(date.getDate());
+    if (props.transportTime === "주간") {
+      pickUpDateTime =
+        year +
+        "-" +
+        (month.length < 2 ? (month = "0" + month) : month) +
+        "-" +
+        (day.length < 2 ? (day = "0" + day) : day) +
+        "T12:00:00";
+    } else {
+      pickUpDateTime =
+        year +
+        "-" +
+        (month.length < 2 ? (month = "0" + month) : month) +
+        "-" +
+        (day.length < 2 ? (day = "0" + day) : day) +
+        "T22:00:00";
+    }
+
+    setData((prevData) => ({
+      ...prevData,
+      pickUpDateTime,
+    }));
+  }, [props.transportTime]);
+
+  useEffect(() => {
+    let workType: string;
+
+    if (props.workType === "차량만") {
+      workType = "DRIVE";
+    } else if (props.workType === "상하차") {
+      workType = "LOADING";
+    } else if (props.workType === "상하차 및 운반") {
+      workType = "CARRY";
+    } else if (props.workType === "기사 + 인부 1명") {
+      workType = "WITH_WORKER";
+    }
+
+    setData((prevData) => ({
+      ...prevData,
+      workType,
+    }));
+  }, [props.workType]);
 
   useEffect(() => {
     axios
@@ -57,7 +106,7 @@ const Cost = (props: costType) => {
       .catch((error) => {
         console.log(error);
       });
-  }, [cost]);
+  }, [data]);
 
   return <div>{cost}원</div>;
 };
